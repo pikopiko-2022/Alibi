17259e010b3c14bce94e49ea6f3eb13ba0ddb4cd
Sheree Simpson <snbsimpson@gmail.com>
2022-09-21 14:32:51 +1200

    adding migration files

---
+++ server/db/migrations/20220921023057_users.js
@@ -0,0 +1,13 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {}
+
+//adding a comment to try make the file show?
