9a6ff8fa7ca50215960beef1759c04c3f773014b
Mac Rawcliffe <107230563+Mac-RawcusJr@users.noreply.github.com>
2022-09-22 11:18:51 +1200

    Update server.js

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ const express = require('express')
 const path = require('path')
 
 const usersRoutes = require('./routes/usersRoute')
+const fruitRoutes = require('./routes/fruits')
+const createRoutes = require('./routes/create')
 
 const server = express()
 
@@ -9,5 +11,9 @@ server.use(express.json())
 server.use(express.static(path.join(__dirname, 'public')))
 
 server.use('/api/v1/users', usersRoutes)
+server.use('/api/v1/fruits', fruitRoutes)
+server.use('/api/v1/create', createRoutes)
 
 module.exports = server
+const express = require('express')
+const path = require('path')
