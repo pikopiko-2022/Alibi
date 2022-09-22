aeb88cda128ffe428412dbb3bb9460e4f213ec31
maneesh-naidu <107604337+maneesh-naidu@users.noreply.github.com>
2022-09-23 11:36:26 +1200

    added post route test for registration

--- server/routes/__tests__/registration.test.js
+++ server/routes/__tests__/registration.test.js
@@ -1,6 +1,6 @@
 const request = require('supertest')
 const server = require('../../server')
-const { getUsers } = require('../../db/dbUsers')
+const { getUsers, addUser } = require('../../db/dbUsers')
 // const { newUser } = require('../../../client/apis/authentication')
 
 jest.mock('../../db/dbUsers')
@@ -22,15 +22,45 @@ describe('GET /api/v1/registration', () => {
         img_url: 'https://pbs.twimg.com/media/EVU8UYAUEAI-csw.jpg',
         rating: 2,
       },
+      {
+        id: 1,
+        auth0_id: 2,
+        flat_id: 1,
+        name: 'Barty',
+        description: 'Energetic and clean',
+        img_url:
+          'https://imageresizer.static9.net.au/FUR-nf6ZUQBmQ_sBZvb3nRpSy58=/400x0/https%3A%2F%2Fprod.static9.net.au%2Ffs%2F06d1a684-e25e-47e9-98ce-9b18323a0f0e',
+        rating: 5,
+      },
     ]
 
     getUsers.mockReturnValue(Promise.resolve(fakeUser))
 
+    expect.assertions(3)
+
     return request(server)
       .get('/api/v1/registration')
       .then((res) => {
-        console.log(res.body)
-        expect(res.body[0].name).toBe('Holloway')
+        expect(res.body[0].name).toBe('Holloway'),
+          expect(res.body).toHaveLength(2),
+          expect(res.body[0].description).toContain('lazy')
+      })
+  })
+})
+
+describe('POST /api/v1/registration', () => {
+  it('Post new user to database', () => {
+    addUser.mockImplementation(() => Promise.resolve(true))
+
+    return request(server)
+      .post('/api/v1/registration')
+      .send({
+        flat_id: '1',
+        name: 'Homer',
+      })
+      .then((res) => {
+        expect(res.body).toBeTruthy()
+        return null
       })
   })
 })
