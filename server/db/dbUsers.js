f76635edbacdcdddb9995a72f6b775642c9ae14b
Mac Rawcliffe <107230563+Mac-RawcusJr@users.noreply.github.com>
2022-09-24 13:54:11 +1200

    tidy up

--- server/db/dbUsers.js
+++ server/db/dbUsers.js
@@ -6,7 +6,9 @@ function getUsers(db = connection) {
 }
 
 function addUser(newUser, db = connection) {
-  return db('users').insert(newUser)
+  return db('users')
+    .insert(newUser)
+    .then(() => getUsers())
 }
 
 function userExists(username, db = connection) {
