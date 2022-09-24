b94e68322240445dc83e95bc20ed2cfb2bca414c
Sheree Simpson <snbsimpson@gmail.com>
2022-09-25 12:55:41 +1300

    back-end lifeG and fix typos in seeds

--- server/routes/lifeGRoute.js
+++ server/routes/lifeGRoute.js
@@ -13,4 +13,16 @@ router.get('/', (req, res) => {
     })
 })
 
+router.get('/issue/:id', (req, res) => {
+  const issue_id = req.params.id
+  db.getLifeGforIssue(issue_id)
+    .then((directive) => {
+      res.json(directive)
+      return null
+    })
+    .catch((err) => {
+      res.status(500).send(err.message)
+    })
+})
+
 module.exports = router
