f95f62c153b7be7c9f11e3b92d707f76c6c59f0c
maneesh-naidu <107604337+maneesh-naidu@users.noreply.github.com>
2022-09-25 23:35:32 +1300

    updated registration to send description and img data

--- server/routes/registration.js
+++ server/routes/registration.js
@@ -16,12 +16,14 @@ router.get('/', checkJwt, (req, res) => {
 
 router.post('/', checkJwt, (req, res) => {
   const auth0_id = req?.user?.sub
-  const { username, flatId } = req.body
+  const { username, flatId, description, img_url } = req.body
 
   const userDetails = {
     auth0_id,
     name: username,
     flat_id: flatId,
+    description: description,
+    img_url: img_url,
   }
 
   db.userExists(username)
