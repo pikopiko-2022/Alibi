df64f68d0fa49c38cd32622067fc9a3efa5f1792
Mac Rawcliffe <107230563+Mac-RawcusJr@users.noreply.github.com>
2022-09-25 19:21:40 +1300

    message and question clen up

--- client/components/Messages.jsx
+++ client/components/Messages.jsx
@@ -1,10 +1,6 @@
-import React, { useEffect } from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import { fetchMessages, addMessage } from '../actions/messages'
+import React from 'react'
 import Question from './Question'
-import LifeGuidance from './LifeGuidance'
 import styles from './Messages.module.scss'
-import Question from './Question'
 
 const Messages = () => {
   return (
