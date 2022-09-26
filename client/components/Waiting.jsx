158a3cae4e0be2c89fc3830e9a2249722f9cb545
Sarah-Quinn-Manaia-22 <107789429+ssaquinn@users.noreply.github.com>
2022-09-27 10:19:33 +1300

    change to endgame

--- client/components/Waiting.jsx
+++ client/components/Waiting.jsx
@@ -1,41 +1,40 @@
 import React, { useState, useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import { useResolvedPath, useNavigate } from 'react-router-dom'
-import { fetchUsers } from '../actions/users'
+import { useNavigate } from 'react-router-dom'
+import { fetchFlatmates } from '../actions/flatmates'
+// eslint-disable-next-line import/no-named-as-default
+import io from 'socket.io-client'
 
 const Waiting = () => {
-  const token = useSelector((state) => state.user?.token)
   const flatmates = useSelector((state) => state.flatmates)
   const dispatch = useDispatch()
-  const [bomb, setBomb] = useState(false)
   const navigate = useNavigate()
+  const socket = io()
 
   useEffect(() => {
-    socket.on('users updated', () => dispatch(fetchUsers()))
+    socket.on('users updated', () => dispatch(fetchFlatmates()))
   }, [])
 
   const nuclear = () => {
     let count = 0
-    flatmates.array.forEach((element) => {
-      ;(element) => {
-        if (element.had_enough == false) {
-          count++
-        }
+    flatmates.forEach((element) => {
+      if (element.had_enough == false) {
+        count++
       }
     })
 
     if (count == 0) {
       return true
-    }
+    } else return false
   }
 
   useEffect(() => {
-    if (nuclear) navigate('/theend')
+    if (nuclear()) navigate('/theend')
   }, [flatmates])
 
   return (
     <>
-      {bomb ? true : <TheEnd />}:<div>Your Flatmates Still Like You.......</div>
+      <div>Your Flatmates Still Like You.......</div>
       <div>Please Wait</div>
     </>
   )
