3d4367e8be049017f343456f9a078b0590ff4b18
Ben <8870073+ben-dodd@users.noreply.github.com>
2022-09-27 00:23:51 +1300

    Test Message component

--- client/components/__tests__/Message.test.js
+++ client/components/__tests__/Message.test.js
@@ -19,7 +19,7 @@ describe('<Message />', () => {
       ],
     }),
   }
-  it('displays a message that was sent by a user', () => {
+  it('displays a message that was sent by yourself to another user', () => {
     render(
       <Provider store={fakeStore}>
         <Message
@@ -27,6 +27,7 @@ describe('<Message />', () => {
             id: 200,
             sender_id: 1,
             recipient_id: 3,
+            message: 'Hey Alice',
             date_sent: new Date('2022-04-22 12:51:00'),
           }}
         />
@@ -34,10 +35,74 @@ describe('<Message />', () => {
     )
     const datetime = screen.getByRole('time')
     expect(datetime.innerHTML).toBe('22 April 2022 at 12:51:00 pm')
-    screen.debug()
-    const link = screen.getByRole('link')
-    expect(link.innerHTML).toBe('Learn More!')
-    expect(link.href).toBe('http://www.cnn.com/')
-    expect(link.target).toBe('_blank')
+    const sentTo = screen.getByText('Sent to', { exact: false })
+    expect(sentTo.innerHTML).toBe('Sent to Alice')
+    const message = screen.getByText('Hey Alice')
+    expect(message).toBeDefined()
+  })
+  it('displays a message that to you by another user', () => {
+    render(
+      <Provider store={fakeStore}>
+        <Message
+          message={{
+            id: 200,
+            sender_id: 3,
+            recipient_id: 1,
+            message: 'Hey Friend',
+            date_sent: new Date('2022-04-22 12:51:00'),
+          }}
+        />
+      </Provider>
+    )
+    const datetime = screen.getByRole('time')
+    expect(datetime.innerHTML).toBe('22 April 2022 at 12:51:00 pm')
+    const sentBy = screen.getByText('Alice')
+    expect(sentBy).toBeDefined()
+    const sentTo = screen.getByText('Sent to', { exact: false })
+    expect(sentTo.innerHTML).toBe('Sent to You')
+    const message = screen.getByText('Hey Friend')
+    expect(message).toBeDefined()
+  })
+  it('displays a message that you sent to everyone', () => {
+    render(
+      <Provider store={fakeStore}>
+        <Message
+          message={{
+            id: 200,
+            sender_id: 1,
+            message: 'Hey Everyone',
+            date_sent: new Date('2022-04-22 12:51:00'),
+          }}
+        />
+      </Provider>
+    )
+    const datetime = screen.getByRole('time')
+    expect(datetime.innerHTML).toBe('22 April 2022 at 12:51:00 pm')
+    const sentTo = screen.getByText('Sent to', { exact: false })
+    expect(sentTo.innerHTML).toBe('Sent to Everyone')
+    const message = screen.getByText('Hey Everyone')
+    expect(message).toBeDefined()
+  })
+  it('displays a message that another user sent to everyone', () => {
+    render(
+      <Provider store={fakeStore}>
+        <Message
+          message={{
+            id: 200,
+            sender_id: 4,
+            message: 'Hey Everyone, my name is Hannah',
+            date_sent: new Date('2022-04-22 12:51:00'),
+          }}
+        />
+      </Provider>
+    )
+    const datetime = screen.getByRole('time')
+    expect(datetime.innerHTML).toBe('22 April 2022 at 12:51:00 pm')
+    const sentBy = screen.getByText('Hannah')
+    expect(sentBy).toBeDefined()
+    const sentTo = screen.getByText('Sent to', { exact: false })
+    expect(sentTo.innerHTML).toBe('Sent to Everyone')
+    const message = screen.getByText('Hey Everyone, my name is Hannah')
+    expect(message).toBeDefined()
   })
 })
