664f83a6bc220ae844d5b06854a2775b154b0952
Sarah-Quinn-Manaia-22 <107789429+ssaquinn@users.noreply.github.com>
2022-09-26 13:33:34 +1300

    add answers database test

---
+++ server/db/__tests__/dbAnswers.test.js
@@ -0,0 +1,32 @@
+const knex = require('knex')
+const config = require('../knexfile')
+const testCon = knex(config.test)
+
+const { getAllAnswers } = require('../dbAnswers')
+// getAnswers, getAnswersForQuestions
+
+beforeAll(() => {
+  return testCon.migrate.latest()
+})
+
+beforeEach(() => {
+  return testCon.seed.run()
+})
+
+afterAll(() => {
+  testCon.destroy()
+})
+
+describe('getAllAnswers', () => {
+  it('gets all of the answers', () => {
+    return getAllAnswers(testCon).then((data) => {
+      expect(data).toHaveLength(30)
+    })
+  })
+})
+
+// describe('getAnswers', () => {
+//   it('gets answers to corresponding question_id', () => {
+//     return
+//   })
+// })
