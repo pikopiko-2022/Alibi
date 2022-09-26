ea9ed36fef27a93a91c1d26747900722204ce33d
Sarah-Quinn-Manaia-22 <107789429+ssaquinn@users.noreply.github.com>
2022-09-27 10:08:38 +1300

    pull from dev

--- client/components/Waiting.jsx
+++ client/components/Waiting.jsx
@@ -1,33 +1,41 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import { useResolvedPath } from 'react-router-dom'
+import { useResolvedPath, useNavigate } from 'react-router-dom'
 import { fetchUsers } from '../actions/users'
 
 const Waiting = () => {
   const token = useSelector((state) => state.user?.token)
-  const users = useSelector((state)=> state.users)
+  const flatmates = useSelector((state) => state.flatmates)
   const dispatch = useDispatch()
   const [bomb, setBomb] = useState(false)
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    socket.on('users updated', () => dispatch(fetchUsers()))
+  }, [])
 
   const nuclear = () => {
-    const count = 0
-    dispatch(fetchUsers(token))=>{
-      users.map((item)=>{
-        if(item.had_enough == true ){
+    let count = 0
+    flatmates.array.forEach((element) => {
+      ;(element) => {
+        if (element.had_enough == false) {
           count++
-          if(count == users.length){
-setBomb(true)
-          }
         }
-      })
+      }
+    })
 
+    if (count == 0) {
+      return true
     }
-    
   }
 
+  useEffect(() => {
+    if (nuclear) navigate('/theend')
+  }, [flatmates])
+
   return (
-    <>{bomb ? true : <TheEnd/>}:
-      <div>Your Flatmates Still Like You.......</div>
+    <>
+      {bomb ? true : <TheEnd />}:<div>Your Flatmates Still Like You.......</div>
       <div>Please Wait</div>
     </>
   )
