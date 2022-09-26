d4049800f440e9f2cc5b6122b6b23b6dc4bbed38
Mac Rawcliffe <107230563+Mac-RawcusJr@users.noreply.github.com>
2022-09-27 08:40:35 +1300

    removed unused function

--- server/db/dbAnswers.js
+++ server/db/dbAnswers.js
@@ -8,13 +8,4 @@ function getAnswers(id, db = connection) {
   return db('answers').select().where('question_id', id)
 }
 
-function getAnswersForQuestions(questions, db = connection) {
-  const ids = questions.map((question) => question.question_id)
-  return db('answers').select().whereIn('question_id', ids)
-}
-
-module.exports = { getAnswers, getAllAnswers, getAnswersForQuestions }
-
-//get questions - all questions
-//get answers - all answers
-//route get(quetions.id.params), get question at id, get answers at question_id
+module.exports = { getAnswers, getAllAnswers }
