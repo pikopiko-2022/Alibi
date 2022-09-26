2417eef581cfe883d11ff1f5657f9b1ccb066fe8
sheree-simpson <79950707+snickley@users.noreply.github.com>
2022-09-26 18:53:24 +1300

    add test for getAnswers route

--- server/routes/__tests__/answersRoute.test.js
+++ server/routes/__tests__/answersRoute.test.js
@@ -32,3 +32,29 @@ describe('GET /api/v1/answers', () => {
       })
   })
 })
+
+describe('GET answers for questions used', () => {
+  it('retrieves answers that correspond to the correct question id', () => {
+    const fakeQuestionId = 1
+    const fakeAnswersforQuestion = [
+      {
+        id: 3,
+        question_id: 1,
+        answer: 'This morning at the gym',
+        is_bad: 0,
+        is_alibi: 1,
+      },
+      { id: 4, question_id: 1, answer: 'Last night', is_bad: 0, is_alibi: 0 },
+      { id: 5, question_id: 2, answer: 'Squeaky', is_bad: 1, is_alibi: 0 },
+    ]
+
+    getAnswers.mockReturnValue(Promise.resolve(fakeAnswersforQuestion))
+
+    return request(server)
+      .get(`/api/v1/answers/${fakeQuestionId}`)
+      .then((res) => {
+        expect(res.body).toHaveLength(3)
+        expect(res.body[1].answer).toContain('Last night')
+      })
+  })
+})
