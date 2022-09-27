3aa2b1725fa3f5d9ee71bf8c13bcc5a3db34493a
Brayden Drummond <107587496+brayden-drummond@users.noreply.github.com>
2022-09-27 17:16:44 +1300

    update

--- client/apis/__tests__/complaintsApi.test.js
+++ client/apis/__tests__/complaintsApi.test.js
@@ -1,17 +1,12 @@
 import nock from 'nock'
 import { addCulpritToComplaint, createComplaint } from '../complaintsApi'
 const mockUrl = 'http://mockUrl?string=something'
-const mockComplaint = [
-  {
-    id: 1,
-    issue_id: 1,
-    image: null,
-    complaint_raised_by: 1,
-    date_raised: new Date(Date.now()),
-    culprit_id: null,
-    resolved: 0,
-  },
-]
+const mockComplaint = {
+  issue_id: 1,
+  image: null,
+  complaint_raised_by: 1,
+}
+
 const errorMessage = 'something went wrong'
 
 const complaintId = 1
@@ -20,6 +15,7 @@ jest.spyOn(console, 'error')
 afterEach(() => {
   console.error.mockReset()
 })
+
 describe('GET /api/v1/imageUrl', () => {
   it('gets url from AWS and sends form data to database', () => {
     const scope = nock('http://localhost')
@@ -33,11 +29,8 @@ describe('GET /api/v1/imageUrl', () => {
     const scope3 = nock('http://localhost')
       .post('/api/v1/complaints')
       .reply(200, mockComplaint)
-    return createComplaint({
-      issue_id: '1',
-      complaint_raised_by: '1',
-      image: 'mockUrl',
-    }).then((result) => {
+    return createComplaint(mockComplaint).then((result) => {
+      console.log(result)
       expect(result).toEqual(mockComplaint)
       expect(scope.isDone()).toBe(true)
       expect(scope2.isDone()).toBe(true)
@@ -61,11 +54,13 @@ describe('GET /api/v1/imageUrl', () => {
   })
 })
 
-describe('PUT /api/v1/complaints/id', () => {
+describe('PUT /api/v1/complaints/:id', () => {
   it('adds culprit to complaint based on complaintId', () => {
-    const scope = nock('http://localhost').put(
-      `/api/v1/complaints/${complaintId}`
-    )
+    const scope = nock('http://localhost')
+      .put(`/api/v1/complaints/${complaintId}`)
+      .reply(200, JSON.stringify(complaintId), {
+        'Content-Type': 'application/json',
+      })
     return addCulpritToComplaint(complaintId).then(() => {
       console.log(complaintId)
       expect(complaintId).toBe(1)
---
+++ client/apis/__tests__/messagesApi.test.js
@@ -0,0 +1,114 @@
+import nock from 'nock'
+import {
+  addAnswerToMessage,
+  getMessages,
+  sendCustomMessage,
+} from '../messagesApi'
+
+const mockCustomMessage = {
+  id: 1,
+  message: 'Hello',
+  recipient_id: 2,
+}
+
+const mockMessages = [
+  {
+    id: 3,
+    message: 'Hi there',
+    recipient_id: 1,
+  },
+  {
+    id: 4,
+    message: 'How are you?',
+    recipient_id: 2,
+    sender_id: 3,
+  },
+]
+
+jest.spyOn(console, 'error')
+afterEach(() => {
+  console.error.mockReset()
+})
+
+const errorMessage = 'something went wrong'
+
+//sendCustomMessage()
+describe('POST /api/v1/messages', () => {
+  it('sends custom message', () => {
+    const scope = nock('http://localhost')
+      .post('/api/v1/messages')
+      .reply(200, JSON.stringify(mockCustomMessage), {
+        'Content-Type': 'application/json',
+      })
+    return sendCustomMessage(mockCustomMessage).then((result) => {
+      expect(result).toStrictEqual(mockCustomMessage)
+      expect(scope.isDone()).toBe(true)
+    })
+  })
+  it('returns error message when custom message fails', () => {
+    const scope = nock('http://localhost')
+      .get('/api/v1/messages')
+      .replyWithError(errorMessage)
+
+    return getMessages().then(() => {
+      console.error.mockImplementation(() => {})
+      expect(scope.isDone()).toBe(true)
+      expect(console.error).toHaveBeenCalledWith(errorMessage)
+    })
+  })
+})
+
+//getMessages()
+describe('GET /api/v1/messages', () => {
+  it('returns all messages', () => {
+    const scope = nock('http://localhost')
+      .get('/api/v1/messages')
+      .reply(200, JSON.stringify(mockMessages), {
+        'Content-Type': 'application/json',
+      })
+    return getMessages().then((result) => {
+      expect(result).toStrictEqual(mockMessages)
+      expect(scope.isDone()).toBe(true)
+    })
+  })
+  it('returns error message when it fails', () => {
+    const scope = nock('http://localhost')
+      .get('/api/v1/messages')
+      .replyWithError(errorMessage)
+
+    return getMessages().then(() => {
+      console.error.mockImplementation(() => {})
+      expect(scope.isDone()).toBe(true)
+      expect(console.error).toHaveBeenCalledWith(errorMessage)
+    })
+  })
+})
+
+const messageId = 1
+const answerId = 1
+
+//addAnswerToMessage
+describe('PUT /api/v1/messages/:id', () => {
+  it('adds answer to message based on id', () => {
+    const scope = nock('http://localhost')
+      .put(`/api/v1/messages/${messageId}`)
+      .reply(200, JSON.stringify(answerId), {
+        'Content-Type': 'application/json',
+      })
+    return addAnswerToMessage(messageId, answerId).then((result) => {
+      expect(result).toStrictEqual(answerId)
+      expect(scope.isDone()).toBe(true)
+    })
+  })
+  it('returns error message when custom message fails', () => {
+    const scope = nock('http://localhost')
+      .get('/api/v1/messages')
+      .replyWithError(errorMessage)
+
+    return getMessages().then(() => {
+      console.error.mockImplementation(() => {})
+      expect(scope.isDone()).toBe(true)
+      expect(console.error).toHaveBeenCalledWith(errorMessage)
+    })
+  })
+})
