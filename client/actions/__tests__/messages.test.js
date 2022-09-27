d14b1ea150f82775ab8285b4421ce6cbaa3d1d8a
Brayden Drummond <107587496+brayden-drummond@users.noreply.github.com>
2022-09-27 19:17:36 +1300

    updated actions tests

---
+++ client/actions/__tests__/messages.test.js
@@ -0,0 +1,53 @@
+import { setMessages, fetchMessages, SET_MESSAGES } from '../messages'
+
+import { getMessages } from '../../apis/messagesApi'
+
+jest.mock('../../apis/messagesApi')
+
+jest.spyOn(console, 'error')
+
+const mockMessages = [
+  {
+    id: 3,
+    message: 'Hi there',
+    recipient_id: 1,
+  },
+  {
+    id: 4,
+    message: 'How are you?',
+    recipient_id: 2,
+    sender_id: 3,
+  },
+]
+
+getMessages.mockReturnValue(Promise.resolve(mockMessages))
+
+const fakeDispatch = jest.fn()
+
+beforeEach(() => {
+  jest.clearAllMocks()
+})
+
+describe('setMessages', () => {
+  it('sets the messages to be the messages', () => {
+    expect(setMessages(mockMessages).type).toBe(SET_MESSAGES)
+    expect(setMessages(mockMessages).payload).toBe(mockMessages)
+  })
+})
+
+describe('fetchMessages', () => {
+  it('dispatches fetchMessages after api call', () => {
+    return fetchMessages(mockMessages)(fakeDispatch).then(() => {
+      const fakeDispatchAction = fakeDispatch.mock.calls[0][0]
+      expect(fakeDispatchAction.type).toBe(SET_MESSAGES)
+      expect(fakeDispatchAction.payload).toEqual(mockMessages)
+    })
+  })
+  it('Should console.error if request fails', () => {
+    console.error.mockImplementation(() => {})
+    getMessages.mockImplementation(() => Promise.reject(new Error('error')))
+    return fetchMessages()(fakeDispatch).then(() => {
+      expect(console.error).toHaveBeenCalledWith('error')
+    })
+  })
+})
---
+++ client/actions/__tests__/user.test.js
@@ -0,0 +1,103 @@
+import {
+  updateLoggedInUser,
+  UPDATE_LOGGED_IN_USER,
+  clearLoggedInUser,
+  CLEAR_LOGGED_IN_USER,
+  fetchUser,
+  updateUserScore,
+  updateUserEnough,
+} from '../user'
+
+import { getUser, addUserScore, addUserEnough } from '../../apis/userApi'
+
+const mockUserToSave = {
+  id: 1,
+  auth0_id: 1,
+  flat_id: 1,
+  name: 'Gertrude',
+  description: 'lazy and selfish',
+  img_seed: 'https://pbs.twimg.com/media/EVU8UYAUEAI-csw.jpg',
+  rating: 5,
+  had_enough: false,
+}
+
+jest.mock('../../apis/userApi')
+
+jest.spyOn(console, 'error')
+
+getUser.mockReturnValue(Promise.resolve(mockUserToSave))
+addUserScore.mockReturnValue(Promise.resolve(mockUserToSave))
+addUserEnough.mockReturnValue(Promise.resolve(mockUserToSave))
+
+const fakeDispatch = jest.fn()
+
+beforeEach(() => {
+  jest.clearAllMocks()
+})
+
+describe('updateLoggedInUser', () => {
+  it('sets the logged in user to be the user', () => {
+    expect(updateLoggedInUser(mockUserToSave).type).toBe(UPDATE_LOGGED_IN_USER)
+    expect(updateLoggedInUser(mockUserToSave).payload).toBe(mockUserToSave)
+  })
+})
+
+describe('clearLoggedInUser', () => {
+  it('clears the logged in user', () => {
+    expect(clearLoggedInUser().type).toBe(CLEAR_LOGGED_IN_USER)
+  })
+})
+
+describe('fetchUser', () => {
+  it('dispatches setFlat after api call', () => {
+    return fetchUser(mockUserToSave)(fakeDispatch).then(() => {
+      const fakeDispatchAction = fakeDispatch.mock.calls[0][0]
+      expect(fakeDispatchAction.type).toBe(UPDATE_LOGGED_IN_USER)
+      expect(fakeDispatchAction.payload).toEqual(mockUserToSave)
+    })
+  })
+  it('Should console.error if request fails', () => {
+    console.error.mockImplementation(() => {})
+    getUser.mockImplementation(() => Promise.reject(new Error('error')))
+    return fetchUser()(fakeDispatch).then(() => {
+      expect(console.error).toHaveBeenCalledWith('error')
+    })
+  })
+})
+
+const mockScore = 2
+describe('updateUserScore', () => {
+  it('dispatches updateLoggedInUser after api call', () => {
+    return updateUserScore(mockScore)(fakeDispatch).then(() => {
+      console.log(fakeDispatch.mock.calls[0])
+      const fakeDispatchAction = fakeDispatch.mock.calls[0][0]
+      expect(fakeDispatchAction.type).toBe(UPDATE_LOGGED_IN_USER)
+      expect(fakeDispatchAction.payload).toEqual(mockUserToSave)
+    })
+  })
+  it('Should console.error if request fails', () => {
+    console.error.mockImplementation(() => {})
+    addUserScore.mockImplementation(() => Promise.reject(new Error('error')))
+    return fetchUser()(fakeDispatch).then(() => {
+      expect(console.error).toHaveBeenCalledWith('error')
+    })
+  })
+})
+
+describe('updateUserEnough', () => {
+  // it('dispatches updateLoggedInUser after api call', () => {
+  //   return updateUserEnough()(fakeDispatch).then(() => {
+  //     console.log(fakeDispatch.mock.calls)
+  //     const fakeDispatchAction = fakeDispatch.mock.calls[0][0]
+  //     expect(fakeDispatchAction.type).toBe(UPDATE_LOGGED_IN_USER)
+  //     expect(fakeDispatchAction.payload).toEqual(mockUserToSave)
+  //   })
+  // })
+  it('Should console.error if request fails', () => {
+    console.error.mockImplementation(() => {})
+    addUserEnough.mockImplementation(() => Promise.reject(new Error('error')))
+    return fetchUser()(fakeDispatch).then(() => {
+      expect(console.error).toHaveBeenCalledWith('error')
+    })
+  })
+})
