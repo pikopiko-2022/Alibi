2ccc406a3172c53656672375a21793cf6cbe48fe
Sarah-Quinn-Manaia-22 <107789429+ssaquinn@users.noreply.github.com>
2022-09-27 16:43:30 +1300

    update random quote slideshow

--- client/components/theend/FlatFun.jsx
+++ client/components/theend/FlatFun.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { fetchFlatmates } from '../../actions/flatmates'
+import { useSelector, useDispatch } from 'react-redux'
+import Avatar from '../widgets/Avatar'
 
 const FlatFun = () => {
+  const dispatch = useDispatch()
+  const flatmates = useSelector((state) => state.flatmates)
+  const token = useSelector((state) => state.user?.token)
+  const [randomMate, updateRandomMate] = useState({})
+
+  useEffect(() => {
+    dispatch(fetchFlatmates(token))
+    updateRandomMate(flatmates[Math.floor(Math.random() * flatmates.length)])
+  }, [])
+
   return (
     <div>
-      <h1>Flatting is Fun!</h1>
+      <blockquote>
+        <h1>Flatting is Fun!</h1>
+        <cite>-{randomMate.name}</cite>
+      </blockquote>
+      <div>
+        <Avatar seedData={randomMate.img_seed} size={300} />
+      </div>
     </div>
   )
 }
