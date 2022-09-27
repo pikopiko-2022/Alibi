4b8e0ab82c4dc1b4ad451ffa3f1a24d6896f819a
Sarah-Quinn-Manaia-22 <107789429+ssaquinn@users.noreply.github.com>
2022-09-27 18:01:55 +1300

    add action function for add all complaints

--- client/actions/complaints.js
+++ client/actions/complaints.js
@@ -1,8 +1,30 @@
+import { getAllComplaints } from '../apis/complaintsApi'
+
 export const SET_COMPLAINT = 'SET_COMPLAINT'
+export const SET_COMPLAINTS = 'SET_COMPLAINTS'
 
 export function setComplaint(complaint) {
   return {
     type: SET_COMPLAINT,
     payload: complaint,
+  }
+}
+
+export function setComplaints(complaints) {
+  return {
+    type: SET_COMPLAINTS,
+    payload: complaints,
+  }
+}
+
+export function fetchComplaints() {
+  return (dispatch) => {
+    return getAllComplaints()
+      .then((complaints) => {
+        dispatch(setComplaints(complaints))
+      })
+      .catch((err) => {
+        console.error(err)
+      })
   }
 }
