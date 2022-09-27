fe257b4d3b61afc60773dcdc1b8117e0aecedf52
Brayden Drummond <107587496+brayden-drummond@users.noreply.github.com>
2022-09-27 20:10:25 +1300

    answers

--- client/actions/__tests__/answerActions.test.js
+++ client/actions/__tests__/answerActions.test.js
@@ -1,7 +1,9 @@
-import { SET_ANSWERS, fetchAnswers } from '../answers'
+import { SET_ANSWERS, fetchAnswers, updateCulprit } from '../answers'
 import { getAnswersApi } from '../../apis/answersApi'
+import { addCulpritToComplaint } from '../../apis/complaintsApi'
 
 jest.mock('../../apis/answersApi')
+jest.mock('../../apis/complaintsApi')
 
 const fakeDispatch = jest.fn()
 
@@ -34,6 +36,28 @@ describe('fetchAnswers', () => {
         type: SET_ANSWERS,
         payload: fakeAnswer,
       })
+    })
+  })
+  it('Should console.error if request fails', () => {
+    jest.spyOn(console, 'error')
+    console.error.mockImplementation(() => {})
+    getAnswersApi.mockImplementation(() =>
+      Promise.reject(new Error('Api request Failed'))
+    )
+    return fetchAnswers()(fakeDispatch).then(() => {
+      expect(console.error).toHaveBeenCalledWith('Api request Failed')
+    })
+  })
+})
+
+const complaintId = 1
+describe('updateCulprit', () => {
+  it('dispatches SET_ANSWERS action after updateCulprit', () => {
+    addCulpritToComplaint.mockReturnValue(Promise.resolve(complaintId))
+    return updateCulprit(complaintId)(fakeDispatch).then(() => {
+      const fakeDispatchAction = fakeDispatch.mock.calls[0][0]
+      expect(fakeDispatchAction.type).toBe(SET_ANSWERS)
+      // expect(fakeDispatchAction.payload).toEqual(fakeAnswer)
     })
   })
   it('Should console.error if request fails', () => {
