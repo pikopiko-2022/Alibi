1d3c70c530e112c71b9291c13b1af79311aec7cb
maneesh-naidu <107604337+maneesh-naidu@users.noreply.github.com>
2022-09-28 23:02:09 +1300

    changed signin font to suit logo

--- client/components/SignIn.jsx
+++ client/components/SignIn.jsx
@@ -13,7 +13,7 @@ const SignIn = () => {
   return (
     <div className={styles.signInPage}>
       <div className={styles.signInContainer}>
-        <h1>ALIBI</h1>
+        <h1>alī·b·ī</h1>
         <div className={styles.spacer} />
         <Link to="/" className={styles.actionButton} onClick={handleSignIn}>
           Sign In
