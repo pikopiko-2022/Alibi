a3c0c033d433034713fc9ee8ba509f3d062a2f9c
Ben <8870073+ben-dodd@users.noreply.github.com>
2022-09-29 00:31:12 +1300

    Merge branch 'endvideo' of https://github.com/pikopiko-2022/Alibi into game-dev

--- client/components/theend/FlatFun.jsx
+++ client/components/theend/FlatFun.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { fetchFlatmates } from '../../actions/flatmates'
 import { useSelector, useDispatch } from 'react-redux'
 import Avatar from '../widgets/Avatar'
+import styles from './TheEnd.module.scss'
 
 const FlatFun = () => {
   const dispatch = useDispatch()
@@ -15,10 +16,13 @@ const FlatFun = () => {
   }, [])
 
   return (
-    <div>
+    <div
+      className={styles.slideContainer}
+      style={{ backgroundColor: '#3B1A8C' }}
+    >
       <blockquote>
-        <h1>Flatting is Fun!</h1>
-        <cite>-{randomMate.name}</cite>
+        <h1 style={{ color: 'white' }}>Flatting is Fun!</h1>
+        <cite style={{ color: 'white' }}>-{randomMate.name}</cite>
       </blockquote>
       <div>
         <Avatar seedData={randomMate.img_seed} size={300} />
--- client/components/theend/Milk.jsx
+++ client/components/theend/Milk.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { fetchFlatmates } from '../../actions/flatmates'
 import { useSelector, useDispatch } from 'react-redux'
 import Avatar from '../widgets/Avatar'
+import styles from './TheEnd.module.scss'
 
 const Milk = () => {
   const dispatch = useDispatch()
@@ -15,10 +16,13 @@ const Milk = () => {
   }, [])
 
   return (
-    <div>
+    <div
+      className={styles.slideContainer}
+      style={{ backgroundColor: '#A40080' }}
+    >
       <blockquote>
-        <h1>Who Drank the Last Of My Milk?</h1>
-        <cite>-{randomMate.name}</cite>
+        <h1 style={{ color: 'white' }}>Who Drank the Last Of My Milk?</h1>
+        <cite style={{ color: 'white' }}>-{randomMate.name}</cite>
       </blockquote>
       <div>
         <Avatar seedData={randomMate.img_seed} size={300} />
